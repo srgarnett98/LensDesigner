a83c2048fc3af080e1fe05877e04071d67f2ec47
srgarnett98 <srgarnett98@gmail.com>
2020-03-11 14:48:30 +0000

    Added Ray Object, added comment on SimObj

--- core.cpp
+++ core.cpp
@@ -122,24 +122,56 @@ class vector{
 
 class SimObj {
   /*
-  Base class for most objects.
-  Mainly deals with coordinate transformation, which is needed for all objects
+	Base class for most objects.
+	Mainly deals with coordinate transformation, which is needed for all objects
+
+	Params
+	-------
+
+	vector centre
+		Centre point of the object, or origin if its a ray
+
+	float angle, in radians
+	  The angle to the horizontal of the element.
+		For lenses, 0.0 is vertical, so the normal to the lens in horizontal,
+		and increasing angle rotates clockwise !!!!CHECK!!!!
+		For Light, 0.0 is horizontal, increasing angle rotates clockwise
+
+	Methods
+	--------
+
+	void set_values    (centre, angle)
+		sets the values inputted into the object. Work out how to call on start.
+
+	vector get_centre
+		returns the value for the centre of the object.
+
+	float get_angle
+		returns the value for the angle of the object
+
+	vector global_to_local_coords    (coord)
+		Converts a coordinate from the global reference frame, into the local reference frame
+		AKA the reference fram from which the object is at centre = [0, 0] and angle = 0.0.
+
+	vector local_to_global_coords   (coord)
+		Converts a coordinate from the local reference frame into the global reference frame.
+
   */
 
   public:
     vector centre;
     float angle;
 
-    void set_values(vector centre_, float angle_){
+    void set_values(vector centre_, float angle_ = 0.0){
       centre = centre_;
       angle = angle_;
     }
 
-    vector getCentre(){
+    vector get_centre(){
       return centre;
     }
 
-    float getAngle(){
+    float get_angle(){
       return angle;
     }
 
@@ -174,6 +206,94 @@ class SimObj {
 };
 
 
+class Ray: public SimObj{
+	/*
+		Ray creates a ray object defined by a origin and angle.With this you can get an x
+		coordinate for a given y, or vice versa, as well as set when the line terminates, and creating
+		arrays representing the line for plotting purposes.
+
+	Params:
+	------
+	vector centre : FORMERLY ORIGIN
+		Origin point that the ray spawns from
+
+	angle : float, in radians, [default = 0.76]
+		Clockwise angle to the horizontal axis
+
+	wavelength : float, [Default = 0.76]
+		Wavelength of the ray
+
+	Attributes :
+	---------- -
+	max_coords : [float, float]
+		The maximum point that the ray reaches.Mainly used for drawing rays
+		within their limits
+
+	Methods :
+	------
+	solve_for_x(float y) :
+		Returns the corresponding x coord from a y coord.
+
+	solve_for_y(float x) :
+		Returns the corresponding x coord from a y coord.
+
+	set_max_coords_from_x(float x) :
+		Sets max_coords to the limit of the ray at that x coord
+
+	set_max_coords_from_y(float y) :
+		Sets max_coords to the limit of the ray at that y coord
+
+	*/
+	public:
+		float wavelength;
+		vector max_coords;
+
+	void set_values(vector centre_, float angle_ = 0.0, float wavelength_ = 0.76){
+		centre = centre_;
+		angle = angle_;
+		wavelength = wavelength_;
+	}
+
+	void set_max_coords_from_y(float y_coord_) {
+		max_coords.y = y_coord_;
+		max_coords.x = solve_for_x(y_coord_);
+	}
+
+	void set_max_coords_from_x(float x_coord_) {
+		max_coords.x = x_coord_;
+		max_coords.y = solve_for_y(x_coord_);
+	}
+
+	float get_wavelength() {
+		return wavelength;
+	}
+
+	vector get_max_coords() {
+		return max_coords;
+	}
+
+	float solve_for_x(float y_coord_) {
+		//increase in x using tan(angle) = dy/dx where dy and dx are difference in y and x
+		float dx = (y_coord_ - centre.y) / tan(angle);
+		//add that increase to the starting x to find where the x coord is at that y.
+		float x_coord_ = centre.x + dx;
+
+		return x_coord_;
+	}
+
+	float solve_for_y(float x_coord_) {
+		//increase in y using tan(angle) = dy/dx where dy and dx are difference in y and x
+		float dy = (x_coord_ - centre.x) * tan(angle);
+		//add that increase to the starting y to find where the y coord is at that x.
+		float y_coord_ = centre.y + dy;
+
+		return y_coord_;
+	}
+
+
+
+};
+
 int main()
 {
   /*
@@ -193,6 +313,13 @@ int main()
 
   std::cout << testObj.global_to_local_coords(testVec).x << std::endl;
   std::cout << testObj.global_to_local_coords(testVec).y << std::endl;
+
+  Ray testRay;
+  testRay.set_values(testVec, 1.0);
+
+  std::cout << testRay.solve_for_y(3.0) << std::endl;
+
+
 
   return 0;
 }
