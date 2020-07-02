3f30d961b85329503c769fb9ce15365be49bedf4
sgarnett <srgarnett98@gmail.com>
2020-07-02 22:53:43 +0100

    Added Aperture class. Added exists attribute for rays to replace nan behaviour

--- core.cpp
+++ core.cpp
@@ -252,11 +252,14 @@ class Ray: public SimObj{
 	public:
 		float wavelength;
 		vector max_coords;
+    bool exists;
 
-	void set_values(vector centre_, float angle_ = 0.0, float wavelength_ = 0.76){
-		centre = centre_;
-		angle = angle_;
+	void set_values(vector centre_, float angle_ = 0.0,
+                  float wavelength_ = 0.76,
+                  bool exists_ = true){
+    SimObj::set_values(centre_, angle_);
 		wavelength = wavelength_;
+    exists = exists_;
 	}
 
 	void set_max_coords_from_y(float y_coord_) {
@@ -296,18 +299,18 @@ class Ray: public SimObj{
 	}
 };
 
-class Interface: public SimObj{
+class Surface: public SimObj{
     /*
-    Base class for all interfaces in the simulation. Lens surfaces, flat surface, aperture etc
+    Base class for all Surfaces in the simulation. Lens surfaces, flat surface, aperture etc
 
       Parameters
       ----------
 
           n1:     float or string of valid material
-                  Refractive index or material before the interface
+                  Refractive index or material before the Surface
 
           n2:     float or string of valid material
-                  Refractive index or material after the interface
+                  Refractive index or material after the Surface
 
           centre = [0.0, 0.0]: [float, float]
               Coordinates of the centre of the aperture
@@ -316,30 +319,33 @@ class Interface: public SimObj{
               Angle of the aperture to the vertical
 
           element_height: default np.inf
+              A height of a lens or aperture etc.
+              Past this height the behaviour may be undefined
+
+      Methods
+      ----------
+
+          tangent_angle:  float
+              Calculates the tangent to the Surface at a specified height
     */
     public:
       //n1 and n2 are both functions.
       //They take wavelength as the argument and return refractive index of that wavelength
-      float (*n1)(float);
-      float (*n2)(float);
       float element_height;
 
     void set_values(vector centre_, float angle_ = 0.0,
-                    float (*n1_)(float) = pmma, float (*n2_)(float) = pmma,
                     float element_height_ = NAN){
       centre = centre_;
       angle = angle_;
-      n1 = *n1_;
-      n2 = *n2_;
       element_height = element_height_;
     }
 
     Ray transfer_func(Ray ray_){
-      throw std::logic_error("Interface requires valid transfer function");
+      throw std::logic_error("Surface requires valid transfer function");
     }
 
     float solve_for_x(float y_coord_){
-      throw std::logic_error("Interface requires valid sag function");
+      throw std::logic_error("Surface requires valid sag function");
     }
 
     float tangent_angle(float y_coord_){
@@ -350,6 +356,96 @@ class Interface: public SimObj{
       return tangent;
     }
 
+};
+
+class Aperture: public Surface{
+  /*
+    Creates an object representing an aperture, where light passes through a small
+    hole in the centre
+
+    Params:
+    ---------
+        aperture_size:  float
+            Total width of the aperture
+
+        centre = [0.0, 0.0]: [float, float]
+            Coordinates of the centre of the aperture
+
+        angle = 0.0: float in radians
+            Angle of the aperture to the vertical
+
+        element_height = 20.0: float
+            Width of the surface in the y axis
+
+    Methods:
+    ------
+        global_to_local_coordinates(coords): [float, float]
+            Takes input of global x and y coordinates, and returns the coordinates
+            in the frame of reference of the lens being at the origin and vertical.
+
+        local_to_global_coordinates(coords): [float, float]
+            Takes input of local x and y coordinates, and returns the coordinates
+            in the global frame of reference.
+
+        solve_for_x(y): float
+            From a local value of y, returns the local x coordinate of the surface
+            Always returns 0 because it is flat.
+
+        draw_element(axes = None): matplotlib Axes object
+            Plots the lens surface on a matplotlib axes. Will create new axes
+            by default, but will use the current active figure if passed.
+
+        transformation_by_element(rays): list of list of Ray objects, sorted by source
+            From incident rays, finds which pass through the aperture
+            Returns the set of rays that would result from the incident set being
+            blocked, with blocked rays being nan rays.
+    */
+    public:
+      float aperture_size;
+    void set_values(vector centre_, float angle_,
+                    float element_height_ = NAN,
+                    float aperture_size_ = 1.0){
+      Surface::set_values(centre_, angle_, element_height_);
+      aperture_size = aperture_size_;
+    }
+
+    Ray transfer_func(Ray ray_){
+      /*
+        Pass through aperture
+        Blocks any rays (turns to null) if they don't go through the aperture, else
+        propogates those rays
+
+        Params
+        ------------
+            ray_:       Ray
+                Incident ray on the aperture
+
+        Returns
+        -------------
+            new_ray:    Ray
+                Either the same ray as incident, if it would pass through the aperture,
+                or a nan ray, where all its properties are nans
+        */
+        Ray new_ray;
+        if (!ray_.exists) {
+          return ray_;
+        }
+
+        vector local_coords;
+        local_coords = global_to_local_coords(ray_.centre);
+
+        if (aperture_size / 2.0 > abs(local_coords.y)) {
+          new_ray = ray_;
+        } else {
+          ray_.exists = false;
+          new_ray = ray_;
+        }
+        return new_ray;
+    }
+
+    float solve_for_x(float y) {
+      return 0.0;
+    }
 };
 
 int main()
@@ -376,8 +472,19 @@ int main()
   testRay.set_values(testVec, 1.0);
 
   std::cout << testRay.solve_for_y(3.0) << std::endl;
+  std::cout << testRay.wavelength << std::endl;
 
+  Aperture testAperture;
+  testAperture.set_values(testVec, 0.0);
 
+  std::cout << testAperture.transfer_func(testRay).centre.x << std::endl;
+  std::cout << testAperture.transfer_func(testRay).exists << std::endl;
+
+  vector testVec2;
+  testVec2.set_values(2.5, 5.5);
+  Ray testRay2;
+  testRay2.set_values(testVec2, 1.0);
+  std::cout << testAperture.transfer_func(testRay2).exists << std::endl;
 
   return 0;
 }
