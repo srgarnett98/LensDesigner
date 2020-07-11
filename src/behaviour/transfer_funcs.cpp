90c80bb06dc03471578ebc1f4120a285251f5a3f
sgarnett <srgarnett98@gmail.com>
2020-07-11 18:04:10 +0100

    Proper file organisation: calc_refract_angle

---
+++ src/behaviour/transfer_funcs.cpp
@@ -0,0 +1,48 @@
+#include "behaviour/transfer_funcs.hpp"
+
+float behaviour::calc_refract_angle(float n1, float n2, float theta1){
+  /*
+  Calculates the refracted angle for a ray crossing a material boundary
+
+  Inputs
+  ----------
+  float n1
+    The refractive index of the first material
+
+  float n2
+    The refractive index of the second material
+
+  float theta1
+    The incident angle (angle to perpendicular to the boundary) of the ray
+    Unit: Radians
+
+  Returns
+  ----------
+  float theta2
+    The refracted angle (to perpendicular to the boundary) of the exiting ray
+    Unit: Radians
+  */
+
+  float theta2;
+  bool add_pi;
+
+  try{
+    // Adding pi will make the direction of the beam rotate 180 degrees
+    // This ensures the exiting ray angle is in the forward direction
+    add_pi = (int( (theta1 + M_PI_2)/ M_PI) %2)==1;
+
+    // Snell's law (sin(theta1)/sin(theta2) = n1/n2) rearranged
+    theta2 = asin(sin(theta1) * n1 / n2);
+
+    //Add the Pi to ensure the direction of exiting ray is forward, if necessary
+    // AKA same "direction" as incident ray
+    add_pi ? theta2+= M_PI: false;
+  }
+  catch(...){
+    //If this fails (arcsin returns an error because it isnt valid)
+    //the ray is therefore past the critical angle, and reflects.
+    //TODO: Model this as a reflection, and not as the ray disappearing
+    return NAN;
+  }
+  return theta2;
+}
---
+++ src/behaviour/transfer_funcs.hpp
@@ -0,0 +1,3 @@
+namespace behaviour{
+  float calc_refract_angle(float n1, float n2, float theta1);
+}
