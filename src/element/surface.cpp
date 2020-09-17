f4d552326c06ffa88879264fdd365d81b1561384
srgarnett98 <srgarnett98@gmail.com>
2020-09-17 16:10:09 +0100

    Including transfer_to_surface in surface transfer func

--- src/element/surface.cpp
+++ src/element/surface.cpp
@@ -46,6 +46,8 @@ light::Ray Surface::LensSurface::transfer_func(light::Ray ray) override{
     return ray;
   }
 
+  light::Ray inter_ray = behaviour::transfer_to_surface(ray, this);
+
   float n1_val = n1(ray.wavelength);
   float n2_val = n2(ray.wavelength);
 
@@ -53,12 +55,12 @@ light::Ray Surface::LensSurface::transfer_func(light::Ray ray) override{
   //coord::vector local_coords = global_to_local_coords(ray.centre);
 
   float lens_normal = geometry->normal_angle(local_coords.y);
-  float angle_normal = ray.angle - lens_normal;
+  float angle_normal = inter_ray.angle - lens_normal;
   float angle_refract = behaviour::calc_refract_angle(n1_val, n2_val, angle_normal);
   float new_ray_angle = angle_refract + lens_normal;
 
   light::Ray new_local_ray = Ray();
-  new_local_ray.set-values(ray.centre, new_ray_angle, ray.wavelength, true);
+  new_local_ray.set-values(inter_ray.centre, new_ray_angle, inter_ray.wavelength, true);
 
   light::Ray new_ray;
   new_ray = local_to_global_ray(new_local_ray);
@@ -71,17 +73,19 @@ light::Ray Surface::Mirror::transfer_func(light::Ray ray) override{
     return ray;
   }
 
+  light::Ray inter_ray = behaviour::transfer_to_surface(ray, this);
+
   //ray is not transformed out of global in "transfer to surface"
   //coord::vector local_coords = global_to_local_coords(ray.centre);
 
   float mirror_normal = geometry->normal_angle(local_coords.y);
-  float angle_normal = ray.angle - mirror_normal;
+  float angle_normal = inter_ray.angle - mirror_normal;
   //is calculate reflect angle just ang;e = - angle?
   float angle_refract = behaviour::calc_reflect_angle(angle_normal);
   float new_ray_angle = angle_refract + mirror_normal;
 
   light::Ray new_local_ray = Ray();
-  new_local_ray.set-values(ray.centre, new_ray_angle, ray.wavelength, true);
+  new_local_ray.set-values(inter_ray.centre, new_ray_angle, inter_ray.wavelength, true);
 
   light::Ray new_ray;
   new_ray = local_to_global_ray(new_local_ray);
@@ -106,16 +110,20 @@ light::Ray element::Aperture::transfer_func(light::Ray ray_){
             Either the same ray as incident, if it would pass through the aperture,
             or a nan ray, where all its properties are nans
     */
-    light::Ray new_ray;
-    if (!ray_.exists) {
-      return ray_;
+
+    if (!ray.exists) {
+      return ray;
     }
 
-    if (aperture_size / 2.0 > abs(local_coords.y)) {
-      new_ray = ray_;
+    light::Ray inter_ray = behaviour::transfer_to_surface(ray, this);
+
+    light::Ray new_ray;
+
+    if (aperture_size / 2.0 > abs(inter_ray.centre.y)) {
+      new_ray = inter_ray;
     } else {
       ray_.exists = false;
-      new_ray = local_to_global_ray(ray_);
+      new_ray = ray;
     }
 
     return new_ray;
